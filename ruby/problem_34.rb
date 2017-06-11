23ccbd11153d8f9a17263e7bf67c098116aaefef
Graham <gpaye8@gmail.com>
2017-06-10 21:59:18 -0700

    add ruby problem_34.rb

---
+++ ruby/problem_34.rb
@@ -0,0 +1,20 @@
+def factorial(num)
+  return 1 if num < 1
+  num * (factorial(num - 1))
+end
+
+def digit_factorial_sum(num)
+  digits = num.to_s.split('').map(&:to_i)
+  digits.map! { |x| factorial(x) }
+  digits.reduce(:+) == num
+end
+
+def digit_factorials
+  facts = []
+  (10..999999).each do |num|
+    facts << num if digit_factorial_sum(num)
+  end
+  facts.reduce(:+)
+end
+
+p digit_factorials
