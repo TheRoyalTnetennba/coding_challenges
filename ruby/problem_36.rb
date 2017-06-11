12ad82b3613d46baddc5a5e32bdda16aa2cd4d2b
Graham <gpaye8@gmail.com>
2017-06-10 22:24:53 -0700

    add problem_36 (ruby)

---
+++ ruby/problem_36.rb
@@ -0,0 +1,16 @@
+def double_palindrome?(num)
+  digits = num.to_s.split('').map(&:to_i)
+  binary = num.to_s(2)
+  digits == digits.reverse && binary == binary.reverse
+end
+
+def palindromic_sum(limit = 1000000)
+  palindromic_nums = []
+  (1...limit).each do |num|
+    palindromic_nums << num if double_palindrome?(num)
+  end
+  palindromic_nums.reduce(:+)
+end
+
+
+p palindromic_sum
