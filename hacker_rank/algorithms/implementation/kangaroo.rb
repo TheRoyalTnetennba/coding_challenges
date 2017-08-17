41656eda172d7b410da56a299b1a5aca6c8e2cac
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-17 11:52:13 -0700

    add kangaroo

--- hacker_rank/algorithms/implementation/kangaroo.rb
+++ hacker_rank/algorithms/implementation/kangaroo.rb
@@ -1,20 +1,31 @@
-s,t = gets.strip.split(' ')
-s = s.to_i
-t = t.to_i
-a,b = gets.strip.split(' ')
-a = a.to_i
-b = b.to_i
-m,n = gets.strip.split(' ')
-m = m.to_i
-n = n.to_i
-apple = gets.strip
-apple = apple.split(' ').map(&:to_i)
-orange = gets.strip
-orange = orange.split(' ').map(&:to_i)
-# s < a + d < t
-ha = 0
-ho = 0
-apple.each { |manzana| ha += 1 if (s <= manzana + a) && (manzana + a <= t) }
-orange.each { |naranja| ho += 1 if ( s<= naranja + b) && (naranja + b <= t) }
-puts ha
-puts ho
+def kangaroo(x1, v1, x2, v2)
+  return 'NO' if x1 > x2 && v1 > v2
+  return 'NO' if x2 > x1 && v2 > v1
+  return 'NO' if x2 != x1 && v1 == v2
+
+  arr1 = [x1]
+  arr2 = [x2]
+  if x1 > x2
+    until arr2.max > arr1.max
+      arr1 << arr1[-1] + v1
+      arr2 << arr2[-1] + v2
+    end
+  elsif x2 > x1
+    until arr1.max > arr2.max
+      arr1 << arr1[-1] + v1
+      arr2 << arr2[-1] + v2
+    end
+  else
+    return "YES"
+  end
+  arr1.each_index { |i| return 'YES' if arr1[i] == arr2[i] }
+  'NO'
+end
+
+x1, v1, x2, v2 = gets.strip.split(' ')
+x1 = x1.to_i
+v1 = v1.to_i
+x2 = x2.to_i
+v2 = v2.to_i
+result = kangaroo(x1, v1, x2, v2)
+puts result;
