3663ed954c65145bfdbd27bea334cf2d5ebe649e
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-18 10:36:37 -0700

    add go version of judge route circle

---
+++ leet_code/judgeRouteCircle.go
@@ -0,0 +1,12 @@
+func judgeCircle(moves string) bool {
+  var counter = make(map[rune]int)
+  for i := 0; i < len(moves); i++ {
+      move := rune(moves[i])
+    if _, ok := counter[move]; ok {
+      counter[move] += 1
+    } else {
+      counter[move] = 1
+    }
+  }
+  return counter['D'] == counter['U'] && counter['L'] == counter['R']
+}
