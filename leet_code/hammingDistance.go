f5c767764687d3a53c519dcbff980890049914f7
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-19 08:10:42 -0700

    add hamming distance

---
+++ leet_code/hammingDistance.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func hammingDistance(x int, y int) int {
+	bitArray := intToBitArray(x ^ y)
+	deltas := 0
+	for i := 0; i < len(bitArray); i++ {
+		if bitArray[i] == "1" {
+			deltas++
+		}
+	}
+	return deltas
+}
+
+func intToBitArray(x int) []string {
+	var x64 = int64(x)
+	var bits = strconv.FormatInt(x64, 2)
+	bitArray := strings.Split(bits, "")
+	return bitArray
+}
--- leet_code/judgeRouteCircle.go
+++ leet_code/judgeRouteCircle.go
@@ -1,12 +1,14 @@
+package main
+
 func judgeCircle(moves string) bool {
-  var counter = make(map[rune]int)
-  for i := 0; i < len(moves); i++ {
-      move := rune(moves[i])
-    if _, ok := counter[move]; ok {
-      counter[move] += 1
-    } else {
-      counter[move] = 1
-    }
-  }
-  return counter['D'] == counter['U'] && counter['L'] == counter['R']
+	var counter = make(map[rune]int)
+	for i := 0; i < len(moves); i++ {
+		move := rune(moves[i])
+		if _, ok := counter[move]; ok {
+			counter[move]++
+		} else {
+			counter[move] = 1
+		}
+	}
+	return counter['D'] == counter['U'] && counter['L'] == counter['R']
 }
