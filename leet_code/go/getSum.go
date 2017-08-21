df8749a3c4c7c346281167aed7bfa56c86cb951e
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-20 21:46:58 -0700

    continue getSum

--- leet_code/go/getSum.go
+++ leet_code/go/getSum.go
@@ -1,6 +1,36 @@
 func getSum(a int, b int) int {
-	var slice []int
+	sameSign := addOrNot(a, b)
+	sign := getSign(a, b, sameSign)
 	slice = append(slice, make([]int, a)...)
 	slice = append(slice, make([]int, b)...)
-	return len(slice)
+	if sameSign {
+		var slice []int
+		return len(slice) * sign
+	} else if a < 0 {
+		c := a * -1
+	}
+}
+
+func addOrNot(a int, b int) bool {
+	if (a >= 0 && b >= 0) || (a < 0 && b < 0) {
+		return true
+	}
+	return false
+}
+
+func getSign(a int, b int, sameSign bool) int {
+	if sameSign && a >= 0 {
+		return 1
+	} else if sameSign && a <= 0 {
+		return -1
+	} else if a > b && a >= 0 {
+		return 1
+	} else if a > b && a < 0 {
+		return -1
+	} else if a < b && b >= 0 {
+		return 1
+	} else if a < b && b < 0 {
+		return -1
+	}
+	return 1
 }
