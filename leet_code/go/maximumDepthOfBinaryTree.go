dcf64fc07cde4e64385713656db3e74b56cc785a
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-23 11:15:29 -0700

    add max depth of binary tree

---
+++ leet_code/go/maximumDepthOfBinaryTree.go
@@ -0,0 +1,10 @@
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left, right := maxDepth(root.Left), maxDepth(root.Right)
+	if left > right {
+		return left + 1
+	}
+	return right + 1
+}
