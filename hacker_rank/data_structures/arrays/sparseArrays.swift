8985374971274fa85fe92c75fa166ec20d224f57
TheRoyalTnetennba <gpaye8@gmail.com>
2017-09-09 15:46:26 -0700

    add sparse arrays

---
+++ hacker_rank/data_structures/arrays/sparseArrays.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+let numStrings = Int(readLine()!)!
+var stringsDict = [String: Int]()
+for i in 0..<numStrings {
+    var str = readLine()!
+    if let val = stringsDict[str] {
+        stringsDict[str] = val + 1
+    } else {
+        stringsDict[str] = 1
+    }
+}
+let numQueries = Int(readLine()!)!
+for i in 0..<numQueries {
+    var str = readLine()!
+    if let val = stringsDict[str] {
+        print(val)
+    } else {
+        sprint(0)
+    }
+}
