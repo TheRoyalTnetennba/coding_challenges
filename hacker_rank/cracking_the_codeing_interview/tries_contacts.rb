3b5d058adcb836c63af49f45810cbe9e99839519
graham <gpaye8@gmail.com>
2017-09-17 23:25:07 -0700

    add tries_contact

--- hacker_rank/cracking_the_codeing_interview/tries_contacts.rb
+++ hacker_rank/cracking_the_codeing_interview/tries_contacts.rb
@@ -3,17 +3,14 @@
 n = gets.strip.to_i
 book = []
 finds = []
+starts_with = Hash.new(0)
 for a0 in (0..n-1)
   op,contact = gets.strip.split(' ')
-  book << contact if op == 'add'
-  finds << contact if op == 'find'
-end
-count = Hash.new(0)
-book.each do |word|
-  finds.each do |pre|
-    count[pre] += 1 if word.start_with?(pre)
+  if op == 'add'
+    book << contact
+    (1..contact.size).each do |i|
+      starts_with[contact[0...i]] += 1
+    end
   end
-end
-finds.each do |pre|
-  puts count[pre]
+  puts starts_with[contact] if op == 'find'
 end
