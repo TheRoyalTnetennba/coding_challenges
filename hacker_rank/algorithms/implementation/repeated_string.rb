9d740cda4a262adb89a4ffa8ef598696478075fc
TheRoyalTnetennba <gpaye8@gmail.com>
2018-01-12 15:57:00 -0500

    add repeated_string.rb

---
+++ hacker_rank/algorithms/implementation/repeated_string.rb
@@ -0,0 +1,15 @@
+#!/bin/ruby
+
+def repeatedString(s, n)
+	if n <= s.size
+		return s.chars[0..n].select { |n| n == 'a' }.size
+	else
+		acount = s.chars.select { |n| n == 'a' }.size * (n / s.size)
+		return n % s.size == 0 ? acount : acount + s.chars[0...(n % s.size)].select { |n| n == 'a' }.size
+	end
+end
+
+s = gets.strip
+n = gets.strip.to_i
+result = repeatedString(s, n)
+puts result
